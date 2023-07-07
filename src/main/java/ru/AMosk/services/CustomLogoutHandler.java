5f46652b10277bcd122628ffaf746aea92b79ed6
Andrey Moskovets <moskovets.and@gmail.ru>
2023-07-07 10:51:12 +0900

    added CustomLogoutHandler.class

---
+++ src/main/java/ru/AMosk/services/CustomLogoutHandler.java
@@ -0,0 +1,33 @@
+package ru.AMosk.services;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CustomLogoutHandler implements LogoutHandler {
+
+    private static final String AUTHTOKEN = "auth-token";
+
+    private final TokenInMemory tokenInMemory;
+
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        final String token = request.getHeader(AUTHTOKEN).substring(7);
+
+        if (tokenInMemory.dellTokenUser(token) != null) {
+            log.info("Token {} was deleted from storage", token);
+        } else {
+            log.warn("Token {} was not deleted. There is no such record in token storage", token);
+        }
+    }
+}
