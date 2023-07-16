6c0a7508757c147078ded63194a9be9cb8444904
Andrey Moskovets <moskovets.and@gmail.ru>
2023-07-16 11:14:32 +0900

    added test AuthController

---
+++ src/test/java/ru/AMosk/controller/AuthControllerTest.java
@@ -0,0 +1,46 @@
+package ru.AMosk.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.AMosk.dto.security.AuthRequest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void createAuthenticationToken_returnIsOk() throws Exception {
+        this.mockMvc.perform(
+                        post("/login")
+                                .content(objectMapper.writeValueAsString(new AuthRequest("user@user.ruq", "user")))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk());
+    }
+    @Test
+    public void createAuthenticationToken_returnIsBadRequest() throws Exception {
+        this.mockMvc.perform(
+                        post("/login")
+                                .content(objectMapper.writeValueAsString(new AuthRequest("user@user.ru", "user1")))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isBadRequest());
+    }
+}
