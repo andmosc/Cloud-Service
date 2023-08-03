ddeb298136d4ae8c10817d951946a3aae632e191
Andrey M <Moskovets.And@gmail.com>
2023-08-03 10:01:19 +0900

    update

--- src/main/java/ru/AMosk/dto/FileInfoDto.java
+++ src/main/java/ru/AMosk/dto/FileInfoDto.java
@@ -1,10 +1,12 @@
 package ru.AMosk.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class FileInfoDto {
     private String filename;
     private String size;
